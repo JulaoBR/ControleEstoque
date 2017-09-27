bd3727246c4e5200be4675e1ceed5ad820e4e106
JulaoBR <julioscp2009@hotmail.com>
2017-09-27 15:38:36 -0300

    V1.0

--- PassaTempo/frmPesquisaSaida.Designer.cs
+++ PassaTempo/frmPesquisaSaida.Designer.cs
@@ -31,7 +31,7 @@
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.txtCampoData = new System.Windows.Forms.MaskedTextBox();
+            this.txtCampoData = new System.Windows.Forms.TextBox();
             this.rbDataCarregamento = new System.Windows.Forms.RadioButton();
             this.rbNumPedido = new System.Windows.Forms.RadioButton();
             this.rbNomeCliente = new System.Windows.Forms.RadioButton();
@@ -48,7 +48,7 @@
             // 
             // txtPesquisa
             // 
-            this.txtPesquisa.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.txtPesquisa.Margin = new System.Windows.Forms.Padding(4);
             this.txtPesquisa.TextChanged += new System.EventHandler(this.txtPesquisa_TextChanged);
             // 
             // panel2
@@ -58,6 +58,10 @@
             this.panel2.Controls.SetChildIndex(this.txtPesquisa, 0);
             this.panel2.Controls.SetChildIndex(this.btnPesquisar, 0);
             this.panel2.Controls.SetChildIndex(this.groupBox1, 0);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
             // 
             // panel1
             // 
@@ -69,34 +73,31 @@
             this.groupBox1.Controls.Add(this.rbDataCarregamento);
             this.groupBox1.Controls.Add(this.rbNumPedido);
             this.groupBox1.Controls.Add(this.rbNomeCliente);
-            this.groupBox1.Location = new System.Drawing.Point(5, 4);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Location = new System.Drawing.Point(4, 3);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.groupBox1.Size = new System.Drawing.Size(964, 84);
+            this.groupBox1.Size = new System.Drawing.Size(723, 68);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Opções para pesquisa";
             // 
             // txtCampoData
             // 
-            this.txtCampoData.Location = new System.Drawing.Point(673, 36);
-            this.txtCampoData.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.txtCampoData.Mask = "00/00/0000";
+            this.txtCampoData.Location = new System.Drawing.Point(512, 28);
+            this.txtCampoData.MaxLength = 10;
             this.txtCampoData.Name = "txtCampoData";
-            this.txtCampoData.Size = new System.Drawing.Size(281, 24);
+            this.txtCampoData.ReadOnly = true;
+            this.txtCampoData.Size = new System.Drawing.Size(191, 21);
             this.txtCampoData.TabIndex = 3;
             this.txtCampoData.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.txtCampoData.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.txtCampoData_MaskInputRejected);
+            this.txtCampoData.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCampoData_KeyPress_1);
             // 
             // rbDataCarregamento
             // 
             this.rbDataCarregamento.AutoSize = true;
             this.rbDataCarregamento.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.rbDataCarregamento.Location = new System.Drawing.Point(383, 36);
-            this.rbDataCarregamento.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rbDataCarregamento.Location = new System.Drawing.Point(287, 29);
             this.rbDataCarregamento.Name = "rbDataCarregamento";
-            this.rbDataCarregamento.Size = new System.Drawing.Size(244, 22);
+            this.rbDataCarregamento.Size = new System.Drawing.Size(199, 19);
             this.rbDataCarregamento.TabIndex = 2;
             this.rbDataCarregamento.Text = "DATA DE CARREGAMENTO";
             this.rbDataCarregamento.UseVisualStyleBackColor = true;
@@ -106,10 +107,9 @@
             // 
             this.rbNumPedido.AutoSize = true;
             this.rbNumPedido.ForeColor = System.Drawing.Color.Red;
-            this.rbNumPedido.Location = new System.Drawing.Point(215, 37);
-            this.rbNumPedido.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rbNumPedido.Location = new System.Drawing.Point(161, 30);
             this.rbNumPedido.Name = "rbNumPedido";
-            this.rbNumPedido.Size = new System.Drawing.Size(115, 22);
+            this.rbNumPedido.Size = new System.Drawing.Size(96, 19);
             this.rbNumPedido.TabIndex = 1;
             this.rbNumPedido.Text = "Nº PEDIDO";
             this.rbNumPedido.UseVisualStyleBackColor = true;
@@ -120,10 +120,9 @@
             this.rbNomeCliente.AutoSize = true;
             this.rbNomeCliente.Checked = true;
             this.rbNomeCliente.ForeColor = System.Drawing.Color.Green;
-            this.rbNomeCliente.Location = new System.Drawing.Point(8, 36);
-            this.rbNomeCliente.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rbNomeCliente.Location = new System.Drawing.Point(6, 29);
             this.rbNomeCliente.Name = "rbNomeCliente";
-            this.rbNomeCliente.Size = new System.Drawing.Size(153, 22);
+            this.rbNomeCliente.Size = new System.Drawing.Size(127, 19);
             this.rbNomeCliente.TabIndex = 0;
             this.rbNomeCliente.TabStop = true;
             this.rbNomeCliente.Text = "NOME CLIENTE";
@@ -143,20 +142,19 @@
             dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
             this.gridSaidaCarga.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
             this.gridSaidaCarga.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.gridSaidaCarga.Location = new System.Drawing.Point(5, 5);
-            this.gridSaidaCarga.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gridSaidaCarga.Location = new System.Drawing.Point(4, 4);
             this.gridSaidaCarga.Name = "gridSaidaCarga";
             this.gridSaidaCarga.ReadOnly = true;
             this.gridSaidaCarga.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.gridSaidaCarga.Size = new System.Drawing.Size(1212, 556);
+            this.gridSaidaCarga.Size = new System.Drawing.Size(909, 452);
             this.gridSaidaCarga.TabIndex = 0;
             this.gridSaidaCarga.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.gridSaidaCarga_CellDoubleClick);
             // 
             // frmPesquisaSaida
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.ClientSize = new System.Drawing.Size(1260, 748);
-            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.ClientSize = new System.Drawing.Size(945, 608);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmPesquisaSaida";
             this.Text = "PESQUISA DE SAIDAS ";
             this.Load += new System.EventHandler(this.frmPesquisaSaida_Load);
@@ -173,10 +171,10 @@
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.MaskedTextBox txtCampoData;
         private System.Windows.Forms.RadioButton rbDataCarregamento;
         private System.Windows.Forms.RadioButton rbNumPedido;
         private System.Windows.Forms.RadioButton rbNomeCliente;
         private System.Windows.Forms.DataGridView gridSaidaCarga;
+        private System.Windows.Forms.TextBox txtCampoData;
     }
 }
